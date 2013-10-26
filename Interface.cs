1f8d79bed81c9fe59fc452082ec02452bf2345a2
sruon <lol@hao.com>
2013-10-26 09:12:31 +0200

    Removed Debug code

--- Interface.cs
+++ Interface.cs
@@ -11,12 +11,10 @@ namespace ffxivlib
     {
         protected IntPtr address;
         public U structure;
-
         public void modify<X>(string variable, X value)
         {
             IntPtr tobemodified = IntPtr.Add(address, (int)Marshal.OffsetOf(typeof(U), variable));
-            Console.WriteLine("I need to modify {0}", tobemodified.ToString("X"));
-                try
+               try
                 {
                     var byte_value = typeof(BitConverter).GetMethod("GetBytes", new Type[] { value.GetType() })
                     .Invoke(null, new object[] { value });
