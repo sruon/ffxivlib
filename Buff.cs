672ce2d1d323c3b4f8368f323a880aa8c28fd405
sruon <lol@hao.com>
2013-11-11 21:40:15 +0100

    BuffID is actually a short.

--- Buff.cs
+++ Buff.cs
@@ -5,7 +5,7 @@ namespace ffxivlib
     [StructLayout(LayoutKind.Explicit, Pack = 1)]
     public struct BUFF
     {
-        [MarshalAs(UnmanagedType.I1)] [FieldOffset(0)] public byte BuffID;
+        [MarshalAs(UnmanagedType.I2)] [FieldOffset(0)] public short BuffID;
         [MarshalAs(UnmanagedType.R4)] [FieldOffset(0x4)] public float TimeLeft;
         [MarshalAs(UnmanagedType.I4)] [FieldOffset(0x8)] public int BuffProvider;
     };
