f451b81e23c0fc10305caee6f52de457311a79e1
sruon <lol@hao.com>
2013-11-11 15:46:21 +0100

    Moved Buff structure to its own file

---
+++ Buff.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ffxivlib
+{
+    [StructLayout(LayoutKind.Explicit, Pack = 1)]
+    public struct BUFF
+    {
+        [MarshalAs(UnmanagedType.I1)] [FieldOffset(0)] public byte BuffID;
+        [MarshalAs(UnmanagedType.R4)] [FieldOffset(0x4)] public float TimeLeft;
+        [MarshalAs(UnmanagedType.I4)] [FieldOffset(0x8)] public int BuffProvider;
+    };
+}
