164246adf3ccbc482842beeabcd267c010320add
sruon <lol@hao.com>
2013-11-11 15:47:09 +0100

    Added Buff array to PartyMember

--- PartyMember.cs
+++ PartyMember.cs
@@ -25,25 +25,26 @@ namespace ffxivlib
             [MarshalAs(UnmanagedType.I2)] [FieldOffset(0x64)] public short cTP;
             [MarshalAs(UnmanagedType.I2)] [FieldOffset(0x66)] public short cGP;
             [MarshalAs(UnmanagedType.I2)] [FieldOffset(0x68)] public short mGP;
-            //[MarshalAs(UnmanagedType.LPArray, SizeConst=15)]
-            //[FieldOffset(0x70)]
-            //public STATUSEFFECT[] buffs;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst=15)] [FieldOffset(0x70)] public BUFF[] Buffs;
             [MarshalAs(UnmanagedType.R4)] [FieldOffset(0x1F0)] public float X;
             [MarshalAs(UnmanagedType.R4)] [FieldOffset(0x1F4)] public float Z;
             [MarshalAs(UnmanagedType.R4)] [FieldOffset(0x1F8)] public float Y;
             [MarshalAs(UnmanagedType.I4)] [FieldOffset(0x38C)] public int padding;
         };
-
-        [StructLayout(LayoutKind.Explicit, Pack = 1)]
-        public struct STATUSEFFECT
-        {
-            [MarshalAs(UnmanagedType.I1)] [FieldOffset(0)] public byte buff;
-            [MarshalAs(UnmanagedType.I4)] [FieldOffset(0x8)] public int provider;
-        };
     }
 
     public partial class FFXIVLIB
     {
+        /// <summary>
+        /// Deprecated, use getPartyMemberById
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public PartyMember getPartyMemberInfo(int id)
+        {
+            return getPartyMemberById(id);
+        }
+
         /// <summary>
         ///     This function retrieves a PartyMember by its id in the PartyMember array
         ///     The result might be empty, there is no sanity check at the time
@@ -51,12 +52,12 @@ namespace ffxivlib
         /// <param name="id">Position in the PartyMember Array, use Constants.PARTY_MEMBER_ARRAY_SIZE as your max (exclusive)</param>
         /// <returns>PartyMember object</returns>
         /// <exception cref="System.IndexOutOfRangeException">Out of range</exception>
-        public PartyMember getPartyMemberInfo(int id)
+        public PartyMember getPartyMemberById(int id)
         {
             if (id >= Constants.PARTY_MEMBER_ARRAY_SIZE)
                 throw new IndexOutOfRangeException();
             IntPtr pointer = mr.ResolvePointerPath(Constants.PARTYPTR);
-            pointer = IntPtr.Add(pointer, Marshal.SizeOf(typeof (PartyMember.PARTYMEMBERINFO))*id);
+            pointer = IntPtr.Add(pointer, Marshal.SizeOf(typeof(PartyMember.PARTYMEMBERINFO)) * id);
             var p = new PartyMember(mr.CreateStructFromAddress<PartyMember.PARTYMEMBERINFO>(pointer), pointer);
             return p;
         }
