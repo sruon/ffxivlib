79571d9ff64d6357c70605d81e6f5a4f7d56645f
sruon <lol@hao.com>
2013-11-16 22:21:21 +0100

    Demo project for gathering nodes

---
+++ Demo Projects/GetGatheringNodes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ffxivlib;
+
+namespace GetGatheringNodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FFXIVLIB instance = new FFXIVLIB();
+            var node_list = instance.getEntityByType(TYPE.Gathering);
+            foreach (Entity e in node_list)
+                {
+                    Console.WriteLine("{0} {1}", e.structure.name, e.structure.GatheringStatus);
+                }
+        }
+    }
+}
