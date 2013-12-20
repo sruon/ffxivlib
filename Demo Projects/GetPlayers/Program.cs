6c9072014444c55f982eed1374dc302c4c0af834
sruon <lol@hao.com>
2013-12-20 23:06:25 +0100

    Demo project to get players around us

---
+++ Demo Projects/GetPlayers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ffxivlib;
+
+namespace GetPlayers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FFXIVLIB instance = new FFXIVLIB();
+            var nodeList = instance.GetEntityByType(TYPE.Player);
+            foreach (Entity e in nodeList)
+            {
+                Console.WriteLine("{0} {1}", e.Name, ResourceParser.GetJobShortname(e.Job));
+            }
+            Console.ReadLine();
+        }
+    }
+}
