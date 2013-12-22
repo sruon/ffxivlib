815f50e01d806a4b3ad6336bb5b97a2cbfc82690
sruon <lol@hao.com>
2013-12-22 19:21:11 +0100

    Added quick Benchmark project

---
+++ Tools/Benchmark/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using ffxivlib;
+
+namespace Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FFXIVLIB instance = new FFXIVLIB();
+            int iterations = 100;
+            while (true)
+                {
+                    Stopwatch sw = Stopwatch.StartNew();
+                    for (int i = 0; i < iterations; i++)
+                        {
+                            instance.GetEntityByType(TYPE.Player);
+                        }
+                    sw.Stop();
+                    Console.WriteLine((sw.ElapsedMilliseconds/iterations).ToString());
+                }
+        }
+    }
+}
