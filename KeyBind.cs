28edd5563a69f9e75138a75f6e765e9fee46dba9
Aren Villanueva <arenvillanueva@yomogi-soft.com>
2014-03-01 07:32:54 +1100

    Updates to reflect 2.16

---
+++ KeyBind.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ffxivlib
+{
+	public class KeyBind : BaseObject<KeyBind.KeyBindInfo>
+	{
+		public KeyBind (KeyBindInfo structure, IntPtr address)
+			: base (structure, address)
+		{
+			Initialize ();
+		}
+
+		[StructLayout(LayoutKind.Explicit, Pack = 1)]
+		public struct KeyBindInfo
+		{
+			[MarshalAs(UnmanagedType.AnsiBStr)] [FieldOffset(0x0)] public String keyBindString;
+		};
+	}
+}
+
---
+++ KeyBindList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ffxivlib
+{
+	public class KeyBinds
+	{
+		public KeyBinds ()
+		{
+		}
+
+		public bool sendAction(int actionId)
+		{
+
+			return false;
+		}
+	}
+}
+
